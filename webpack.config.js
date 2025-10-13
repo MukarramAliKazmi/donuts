4b02797d7fe3f914d5088796065c50a3137c5638
Mukarram Kazmi <mukarram.ali.kazmi@gmail.com>
2025-10-13 20:08:26 +0500

    add fonts and update webpack config for font support

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.woff2$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
