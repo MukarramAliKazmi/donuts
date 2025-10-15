9adc74aac0203a0a94b60807244c793dbd811d8d
Mukarram Kazmi <mukarram.ali.kazmi@gmail.com>
2025-10-15 17:34:16 +0500

    add loading animation using body classes and window load event

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import { initRouter } from "./lib/router.js";
 
 initRouter();
 
+window.addEventListener("load", () => {
+  document.body.classList.remove("body-loading");
+  document.body.classList.add("body-loaded");
+});
+
