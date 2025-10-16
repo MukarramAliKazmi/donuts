cb2bdd203ccb2961382a01e0c85b510e6f28f650
Mukarram Kazmi <mukarram.ali.kazmi@gmail.com>
2025-10-17 00:35:41 +0500

    add footer to main layout and update layout styles

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,17 +1,22 @@
 import "../styles/layouts/main.css";
 
 import { header } from "../components/header.js";
+import { footer } from "../components/footer.js";
 
 export function mainLayout() {
   const el     = document.createElement("div");
   el.className = "main-layout";
 
   el.innerHTML = `
-    <div class="header-slot"></div>
+    <div class="header-container">
+      <div class="header-slot"></div>
+    </div>
     <div class="content"></div>
+    <div class="footer-slot"></div>
   `;
 
   el.querySelector(".header-slot").replaceWith(header());
+  el.querySelector(".footer-slot").replaceWith(footer());
 
   return el;
 }
